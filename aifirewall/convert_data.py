cda4e7ba75702a19e50c2cf3148d8ab23ad02192
adusnop <58070093@kmitl.ac.th>
2018-11-23 00:33:33 +0700

    e

--- aifirewall/convert_data.py
+++ aifirewall/convert_data.py
@@ -13,20 +13,17 @@ def create_data_model():
 
     enc = OneHotEncoder(handle_unknown='ignore')
     enc2 = OneHotEncoder(handle_unknown='ignore')
-    list_ipenc = enc.fit(list_ip)
+
     list_action = np.reshape(list_action, (-1, 1))
-    list_actionenc = enc2.fit(list_action)
-    print(list_actionenc)
 
-    list_ipenc = enc.transform(list_ip).toarray()
-    list_actionenc = enc2.transform(list_action).toarray()
+    list_ipenc = enc.fit_transform(list_ip).toarray()
+    list_actionenc = enc2.fit_transform(list_action).toarray()
 
-    print(len(list_ipenc))
     list_iptrain = list_ipenc[:150]
     list_iptest = list_ipenc[150:]
     list_actiontrain = list_actionenc[:150]
     list_actiontest = list_actionenc[150:]
-    print('******')
+
     print(len(list_iptest))
 
     return list_iptrain, list_actiontrain, list_iptest, list_actiontest, list_ip
@@ -35,6 +32,29 @@ def create_data_model():
 create_data_model()
 
 def create_data_input():
-    a =3
+    with open('rules_train.csv', 'r') as f:
+        line = csv.reader(f)
+        list_ip = list(line)
+        list_action = [item.pop(0) for item in list_ip]
+        print(list_action)
+        print(list_ip)
+
+    enc = OneHotEncoder(handle_unknown='ignore')
+    enc2 = OneHotEncoder(handle_unknown='ignore')
+
+    list_action = np.reshape(list_action, (-1, 1))
+
+    list_ipenc = enc.fit_transform(list_ip).toarray()
+    list_actionenc = enc2.fit_transform(list_action).toarray()
+
+    list_iptrain = list_ipenc[:150]
+    list_iptest = list_ipenc[150:]
+    list_actiontrain = list_actionenc[:150]
+    list_actiontest = list_actionenc[150:]
+
+    print(len(list_iptest))
+
+    return list_iptrain, list_actiontrain, list_iptest, list_actiontest, list_ip
+
 create_data_input()
 
